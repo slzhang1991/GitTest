b4f054af936c75d97062616135fcecf79409794a
slzhang <slzhang1991@163.com>
2019-06-20 22:28:56 +0800

    解决冲突

--- src/com/git/test/entity/Git.java
+++ src/com/git/test/entity/Git.java
@@ -1,10 +1,6 @@
 package com.git.test.entity;
 
-<<<<<<< HEAD
 // Git测试类
-=======
-// Git类用于测试
->>>>>>> branch 'master' of https://github.com/slzhang1991/GitTest.git
 public class Git {
 	/**
 	 * 主方法
